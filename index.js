2ac5bdf29f462dd15a6c498885642a317b9ca426
Fludi_RO <ludwig_lasse.rosenberg@student.reutlingen-university.de>
2021-04-13 01:22:48 +0200

    + Database connection astablished

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://CloudUser1:CloudComputingSS21@cloudcomputingcluster.xypsx.mongodb.net/cloudcomputingcluster?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("cloudcomputingcluster").collection("users");
+  console.log("Connected correctly to server1");
+  collection.insertOne({
+    name: "Test"
+  });
+  console.log("Connected correctly to server2");
+
+  // perform actions on the collection object
+  client.close();
+  console.log("Connected correctly to server3");
+});
+/*async function run() {
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db('cloudcomputingcluster')
+    await db.collection('users').insertOne({
+          name: 'Ludwig',
+          alter: 21
+        }
+    )
+  } catch (err) {
+    console.log(err.stack);
+  }
+  finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+*/
+
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -17,14 +52,15 @@ io.on('connection', (socket) => {
 
   socket.on('join', msg => {
     socket.name = msg
-    io.emit('join', socket.name + " has joined the chat");
+    io.emit('hello', socket.name + " has joined the chat");
 
     //has do be reworked!
-    io.emit('hello', socket.name);
+    io.emit('join', socket.name);
   });
 
   socket.on('disconnect', () => {
-    io.emit('join', socket.name + " has left the chat");
+    io.emit('hello', socket.name + " has left the chat");
+  //  io.emit('leave', socket.name);
   });
 });
 
