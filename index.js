6c0fd8ccf66b564a24f81624f69d5ca950e3af26
Ludwig Rosenberg <ludwig_lasse.rosenberg@student.reutlingen-university.de>
2021-06-09 20:58:17 +0200

    undone database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://CloudUser1:CloudComputingSS21@cloudcomputingcluster.xypsx.mongodb.net/cloudcomputingcluster?retryWrites=true&w=majority";
 
 async function run(name) {
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, secure:true });
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   try {
     await client.connect();
     console.log("Connected correctly to server");
