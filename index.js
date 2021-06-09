2cfe03e15a72f15a4f9736f81748218152d78017
Ludwig Rosenberg <ludwig_lasse.rosenberg@student.reutlingen-university.de>
2021-06-09 14:59:13 +0200

    Security Test undone

--- index.js
+++ index.js
@@ -65,35 +65,9 @@ async function hashIt(password){
   return hashed;
 }
 */
-//const helmet = require("helmet");
-const app = require('express')();
-//app.use(helmet());
-
-
-
-const csp = require('content-security-policy');
 const helmet = require("helmet");
-
-
-// content-security-policy
-const cspPolicy = {
-  'report-uri': '/reporting',
-  'default-src': csp.SRC_NONE,
-  'script-src': [ csp.SRC_SELF, csp.SRC_DATA ]
-};
-
-const globalCSP = csp.getCSP(csp.STARTER_OPTIONS);
-const localCSP = csp.getCSP(cspPolicy);
-
-app.use(helmet()); // Add Helmet as a middleware
-app.use(helmet.xssFilter())
-
-// This will apply the security policy to all requests if no local policy is set
-app.use(globalCSP);
-
-// This will apply the local security policy just to this path, overriding the global policy
-app.get('/local', localCSP, (req, res) => {
-});
+const app = require('express')();
+app.use(helmet());
 
 app.enable('trust proxy');
 
